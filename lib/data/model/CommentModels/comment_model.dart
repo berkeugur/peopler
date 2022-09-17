2331a315fdadc1413ba061933f5534081964c9f3
Berke Uğur <berkeugur67@gmail.com>
2022-09-17 04:24:34 +0300

    feed comment like dislike button features

--- lib/data/model/CommentModels/comment_model.dart
+++ lib/data/model/CommentModels/comment_model.dart
@@ -3,6 +3,7 @@ import 'package:peopler/data/model/CommentModels/sub_comment_model.dart';
 
 class Comment {
   String? comment;
+  String? commentID;
   int? liked;
   int? disliked;
   String? fromUserID;
@@ -10,6 +11,7 @@ class Comment {
 
   Comment({
     this.comment,
+    this.commentID,
     this.liked,
     this.disliked,
     this.fromUserID,
@@ -18,6 +20,7 @@ class Comment {
 
   Comment.fromJson(Map<String, dynamic> json) {
     comment = json['comment'];
+    commentID = json['commentID'];
     liked = json['liked'];
     disliked = json['disliked'];
     fromUserID = json['fromUserID'];
@@ -27,6 +30,7 @@ class Comment {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['comment'] = comment;
+    data['commentID'] = commentID;
     data['liked'] = liked;
     data['disliked'] = disliked;
     data['fromUserID'] = fromUserID;
--- lib/presentation/screens/COMMENTS/feed_comments.dart
+++ lib/presentation/screens/COMMENTS/feed_comments.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/firestore.dart';
+import 'package:peopler/business_logic/blocs/UserBloc/bloc.dart';
 import 'package:peopler/core/constants/queries/queries.dart';
 import 'package:peopler/data/model/CommentModels/comment_model.dart';
 
@@ -28,7 +29,7 @@ class _FeedCommentsState extends State<FeedComments> {
           print("firestoreListView");
           Comment comment = commentSnapshot.data();
           return savedUserData[comment.fromUserID] != null
-              ? CommentCard(comment, savedUserData[comment.fromUserID])
+              ? CommentCard(comment, savedUserData[comment.fromUserID], widget.feedID)
               : FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
                   future: FirebaseFirestore.instance.collection("users").doc(comment.fromUserID).get(),
                   builder: (context, userSnapshot) {
@@ -47,7 +48,7 @@ class _FeedCommentsState extends State<FeedComments> {
                           savedUserData.addAll({comment.fromUserID!: userData?.data()});
                         }
 
-                        return CommentCard(comment, userData?.data());
+                        return CommentCard(comment, userData?.data(), widget.feedID);
                     }
                   });
         },
@@ -55,11 +56,28 @@ class _FeedCommentsState extends State<FeedComments> {
     );
   }
 
-  Row CommentCard(Comment comment, Map<String, dynamic>? userData) {
+  // ignore: non_constant_identifier_names
+  Widget CommentCard(Comment comment, Map<String, dynamic>? userData, String feedID) {
     bool _isProfileVisiable = userData?["isProfileVisible"] as bool? ?? false;
 
     ValueNotifier<int> _liked = ValueNotifier(comment.liked ?? 0);
     ValueNotifier<int> _disliked = ValueNotifier(comment.disliked ?? 0);
+    bool? isLiked;
+    bool? isDisliked;
+    getData() async {
+      var commentLikedData =
+          await FirebaseFirestore.instance.collection("users").doc(UserBloc.user!.userID).collection("likedComments").doc(comment.commentID).get();
+      Map<String, dynamic>? _commentLikedData = commentLikedData.data();
+      isLiked = _commentLikedData == null ? false : true;
+//
+//
+//
+      var commentDislikedData =
+          await FirebaseFirestore.instance.collection("users").doc(UserBloc.user!.userID).collection("dislikedComments").doc(comment.commentID).get();
+      Map<String, dynamic>? _commentDislikedData = commentDislikedData.data();
+      isDisliked = _commentDislikedData == null ? false : true;
+    }
+
     return Row(
       children: [
         CircleAvatar(
@@ -84,30 +102,146 @@ class _FeedCommentsState extends State<FeedComments> {
             Text(
               comment.comment ?? "null",
             ),
-            Row(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    _liked.value++;
-                  },
-                  child: ValueListenableBuilder(
-                      valueListenable: _liked,
-                      builder: (context, _, __) {
-                        return Text("${_liked.value} beğeni");
-                      }),
-                ),
-                TextButton(
-                  onPressed: () {
-                    _disliked.value++;
-                  },
-                  child: ValueListenableBuilder(
-                      valueListenable: _disliked,
-                      builder: (context, _, __) {
-                        return Text("${_disliked.value} beğenmeme");
-                      }),
-                ),
-              ],
-            ),
+            FutureBuilder(
+                future: getData(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      return const Text("none");
+                    case ConnectionState.waiting:
+                      return Row(
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "${_liked.value} beğeni",
+                              style: TextStyle(
+                                color: isLiked ?? false ? Colors.green : null,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "${_disliked.value} beğenmeme",
+                              style: TextStyle(
+                                color: isDisliked ?? false ? Colors.redAccent : null,
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    case ConnectionState.active:
+                      return const Text("active");
+                    case ConnectionState.done:
+                      return Row(
+                        children: [
+                          //
+                          //Build LikeButton
+                          //
+                          TextButton(
+                            onPressed: () async {
+                              if (isLiked != null) {
+                                if (isLiked!) {
+                                  //beğeniyi geri alma
+                                  await FirebaseFirestore.instance
+                                      .collection("users")
+                                      .doc(UserBloc.user!.userID)
+                                      .collection("likedComments")
+                                      .doc(comment.commentID)
+                                      .delete();
+                                  await FirebaseFirestore.instance
+                                      .collection('feeds')
+                                      .doc(feedID)
+                                      .collection("comments")
+                                      .doc(comment.commentID)
+                                      .update({"liked": (comment.liked ?? 0) - 1});
+                                  _liked.value--;
+                                } else {
+                                  //yeni beğeni
+                                  await FirebaseFirestore.instance
+                                      .collection("users")
+                                      .doc(UserBloc.user!.userID)
+                                      .collection("likedComments")
+                                      .doc(comment.commentID)
+                                      .set({});
+                                  await FirebaseFirestore.instance
+                                      .collection('feeds')
+                                      .doc(feedID)
+                                      .collection("comments")
+                                      .doc(comment.commentID)
+                                      .update({"liked": (comment.liked ?? 0) + 1});
+                                  _liked.value++;
+                                }
+                              }
+                            },
+                            child: ValueListenableBuilder(
+                                valueListenable: _liked,
+                                builder: (context, _, __) {
+                                  return Text(
+                                    "${_liked.value} beğeni",
+                                    style: TextStyle(
+                                      color: isLiked ?? false ? Colors.green : null,
+                                    ),
+                                  );
+                                }),
+                          ),
+
+                          //
+                          //Build DislikeButton
+                          //
+
+                          TextButton(
+                            onPressed: () async {
+                              if (isDisliked != null) {
+                                if (isDisliked!) {
+                                  //beğeniyi geri alma
+                                  await FirebaseFirestore.instance
+                                      .collection("users")
+                                      .doc(UserBloc.user!.userID)
+                                      .collection("dislikedComments")
+                                      .doc(comment.commentID)
+                                      .delete();
+                                  await FirebaseFirestore.instance
+                                      .collection('feeds')
+                                      .doc(feedID)
+                                      .collection("comments")
+                                      .doc(comment.commentID)
+                                      .update({"disliked": (comment.disliked ?? 0) - 1});
+                                  _disliked.value--;
+                                } else {
+                                  //yeni beğeni
+                                  await FirebaseFirestore.instance
+                                      .collection("users")
+                                      .doc(UserBloc.user!.userID)
+                                      .collection("dislikedComments")
+                                      .doc(comment.commentID)
+                                      .set({});
+                                  await FirebaseFirestore.instance
+                                      .collection('feeds')
+                                      .doc(feedID)
+                                      .collection("comments")
+                                      .doc(comment.commentID)
+                                      .update({"disliked": (comment.disliked ?? 0) + 1});
+                                  _disliked.value++;
+                                }
+                              }
+                            },
+                            child: ValueListenableBuilder(
+                                valueListenable: _disliked,
+                                builder: (context, _, __) {
+                                  return Text(
+                                    "${_disliked.value} beğenmeme",
+                                    style: TextStyle(
+                                      color: isDisliked ?? false ? Colors.redAccent : null,
+                                    ),
+                                  );
+                                }),
+                          ),
+                        ],
+                      );
+                  }
+                }),
           ],
         )
       ],
